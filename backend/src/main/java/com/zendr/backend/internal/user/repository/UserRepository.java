b762beb67b6a16808b4a2ac60364e5d6ba4d5440
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2026-03-01 11:54:35 +0100

    Anotacion en el UseRepository

--- backend/src/main/java/com/zendr/backend/internal/user/repository/UserRepository.java
+++ backend/src/main/java/com/zendr/backend/internal/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.zendr.backend.internal.user.repository;
 
 import com.zendr.backend.internal.user.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     List<User> findByNameContainingIgnoreCase(String username);
