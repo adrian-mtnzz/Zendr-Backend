96560da541a02be01c0607283c835bae668946a7
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2026-05-09 12:09:27 +0200

    Faltaba anotar la colección como "disciplines"

--- backend/src/main/java/com/zendr/backend/internal/discipline/model/Discipline.java
+++ backend/src/main/java/com/zendr/backend/internal/discipline/model/Discipline.java
@@ -3,6 +3,7 @@ package com.zendr.backend.internal.discipline.model;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Data
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 @Getter
 @Setter
 @Builder
+@Document(collection = "disciplines")
 public class Discipline {
     
     @Id
