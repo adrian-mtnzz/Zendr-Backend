eef93a6e67ba19db5301d933313d014531c1d768
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2026-05-11 18:41:16 +0200

    Renombrar atributos del TokenResponse

--- backend/src/main/java/com/zendr/backend/api/controllers/DisciplineController.java
+++ backend/src/main/java/com/zendr/backend/api/controllers/DisciplineController.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/disciplines")
+@RequestMapping("/auth/disciplines")
 @RequiredArgsConstructor
 public class DisciplineController {
     
