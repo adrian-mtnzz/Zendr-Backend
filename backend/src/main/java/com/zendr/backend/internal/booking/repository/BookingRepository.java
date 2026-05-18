946ab93507f54a0b00ea43cdf7e1a1bbbe80c6ce
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2026-05-18 09:42:14 +0200

    Reservar y cancelar reservas

--- backend/src/main/java/com/zendr/backend/internal/booking/repository/BookingRepository.java
+++ backend/src/main/java/com/zendr/backend/internal/booking/repository/BookingRepository.java
@@ -2,6 +2,7 @@ package com.zendr.backend.internal.booking.repository;
 
 import com.zendr.backend.internal.booking.model.Booking;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +14,10 @@ public interface BookingRepository extends MongoRepository<Booking, String> {
     List<Booking> findByUserId(String userId);
     Optional<Booking> findByUserIdAndStatus(String userId, Booking.BookingStatus status);
     Optional<Booking> findByUserIdAndStatusNot(String userId, Booking.BookingStatus status);
-    
+    @Query("{ 'userId': ?0, 'eventId': ?1, 'status': { '$ne': ?2 }, 'deletedAt': null }")
+    Optional<Booking> findByUserIdAndEventIdAndStatusNotAndDeletedAtNull(
+            String userId, String eventId, Booking.BookingStatus status
+    );
     Optional<Booking> findByUserIdAndEventIdAndStatusNot(
             String userId, String eventId, Booking.BookingStatus status
     );
