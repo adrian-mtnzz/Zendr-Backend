3a0b44dff8965f4158112ae413ecd74bbe372bc2
Adrian Martinez <adrian.martinez@bootcamp-upgrade.com>
2026-05-19 13:56:06 +0200

    Cancelación de eventos en cascada al eliminar monitor

--- backend/src/main/java/com/zendr/backend/BackendApplication.java
+++ backend/src/main/java/com/zendr/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.zendr.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableMongoAuditing
+@Profile("prod")
 public class BackendApplication {
 
 	public static void main(String[] args) {
